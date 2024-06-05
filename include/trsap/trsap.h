d5ea710f4c9893a9cc4d4e43810573f4b0015ef2
YetAnotherFyrryMan <damiansatala@gmail.com>
2024-06-05 13:51:36 +0200

    trsap: Added enums

--- include/trsap/trsap.h
+++ include/trsap/trsap.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+typedef enum {
+	TRSAP_ARG_TYPE_FLAG = 0,
+	TRSAP_ARG_TYPE_VALUE,
+	TRSAP_ARG_TYPE_VALUE_OPTIONAL,
+	TRSAP_ARG_TYPE_VALUE2,
+	TRSAP_ARG_TYPE_VALUE2_OPTIONAL
+} trsap_ArgType;
+
 const char* trsap_chop(size_t* argc, const char*** argv);
 
 #endif // _TRSAP_H_
--- include/trsap/trsap.hpp
+++ include/trsap/trsap.hpp
@@ -5,6 +5,14 @@ namespace trsap {
 		#include "trsap.h"
 	}
 
+	enum class ArgType {
+		FLAG             = TRSAP_ARG_TYPE_FLAG,
+		VALUE            = TRSAP_ARG_TYPE_VALUE,
+		VALUE_OPTIONAL   = TRSAP_ARG_TYPE_VALUE_OPTIONAL,
+		VALUE2           = TRSAP_ARG_TYPE_VALUE2,
+		VALUE2_OPTIONAL	 = TRSAP_ARG_TYPE_VALUE2_OPTIONAL	
+	};
+
 	inline const char* chop(size_t* argc, const char*** argv){
 		return trsap_chop(argc, argv);
 	}
