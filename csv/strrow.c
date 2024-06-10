4c7c595090f2cdcb533a847be5d55bc1207c4348
YetAnotherFurryMan <damiansatala@gmail.com>
2024-06-10 14:32:32 +0200

    csav: implemented strrow

---
+++ csv/strrow.c
@@ -0,0 +1,27 @@
+#define _GNU_SOURCE
+#include <csv/csv.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+char* csv_strrow(csv_Row row, char delimeter){
+	size_t len = row.m_Count + 1;
+	for(size_t i = 0; i < row.m_Count; i++){
+		len += strlen(row.m_Values[i]);
+	}
+
+	char* str = (char*) malloc(len);
+	if(!str)
+		return str;
+
+	char* cur = str;
+	for(size_t i = 0; i < row.m_Count; i++){
+		cur = (char*) mempcpy(cur, row.m_Values[i], strlen(row.m_Values[i]));
+		*cur = delimeter;
+	}
+
+	*cur = '\n';
+	str[len] = 0;
+
+	return str;
+}
